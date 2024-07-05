12995743879166698753d6e00c86db2a0362bb78
forna <fornagieri.artur@gmail.com>
2024-07-05 13:58:53 -0300

    Add page content

---
+++ buildbox/src/components/Content/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Content = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #343434;
+`;
+
+export default Content;
