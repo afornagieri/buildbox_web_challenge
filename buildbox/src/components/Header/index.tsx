754983f63cb236cba3e6ddfe910454a0e8015668
forna <fornagieri.artur@gmail.com>
2024-07-05 13:16:43 -0300

    Add page header

---
+++ buildbox/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Header = styled.div`
+  position: fixed;
+  border: none;
+  width: 100%;
+  height: 10%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #2a2a2a;
+`;
+
+export default Header;
